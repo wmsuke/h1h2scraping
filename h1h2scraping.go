8de3a5c8f62aef7ea21da6cb9c762444e3f91666
wmsuke <wmsuke@gmail.com>
2016-01-21 17:58:41 +0900

    configに設定したURLからtitle,h1,h2を取得する

--- h1h2scraping.go
+++ h1h2scraping.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+     "fmt"
+     "github.com/PuerkitoBio/goquery"
+	 "github.com/BurntSushi/toml"
+)
+
+type Config struct {
+    Server ServerConfig
+    Uri    UriConfig
+}
+
+type ServerConfig struct {
+    Host  string        `toml:"host"`
+    Port  string        `toml:"port"`
+}
+
+type UriConfig struct {
+     Path[]string `toml:"path"`
+}
+
+func GetPage(no int, url string) {
+     doc, _ := goquery.NewDocument(url)
+	 if doc != nil {
+		 title := ""
+		 h1 := ""
+		 h2 := ""
+		 if doc.Find("title") != nil {
+			title = doc.Find("title").Text()
+		 }
+		 if doc.Find("h1") != nil {
+			h1 = doc.Find("h1").Text()
+		 }
+		 if doc.Find("h2") != nil {
+			h2 = doc.Find("h2").Text()
+		 }
+		 fmt.Printf("%d\t%s\t%s\t%s\t%s\n", 
+			no + 1, url, title, h1, h2)
+	}
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	var config Config
+    _, err := toml.DecodeFile("config.tml", &config)
+    if err != nil {
+        panic(err)
+    }
+
+	for i, v := range config.Uri.Path {
+		url := "http://" + config.Server.Host + ":" + config.Server.Port + v
+		GetPage(i, url)
+	}
 }
